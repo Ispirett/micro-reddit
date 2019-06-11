2486ae53668405e5dbfa06a4ffb994faaf6e3c0a
Macatuz <armando29@live.com>
2019-06-11 15:29:52 -0400

    Last Approach

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ApplicationRecord
   validates :body , presence: true
   validates :user_id, presence:true
   validates :title, presence:true , length:{maximum: 100}
+  has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  validates  :name,  presence:true, length: {minimum: 3}
-  validates  :password, presence:true
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :password, presence: true
   has_many :posts
+  has_many :comments
 end
