a53e9885b4f3335a3836b01f5392ef87b9ad86e7
Ispire <ispirett@gmail.com>
2019-06-12 13:02:19 -0400

    added password digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates  :name,  presence:true, length: {minimum: 3}
   validates  :password, presence:true
-  has_many :posts
+  has_many :posts, dependent: :destroy
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_11_184958) do
+ActiveRecord::Schema.define(version: 2019_06_12_165602) do
 
   create_table "posts", force: :cascade do |t|
     t.string "body"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 2019_06_11_184958) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
